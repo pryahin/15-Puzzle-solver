a91f71a663a29f7e851650f559193041a27e8145
Vladimir Pryahin <pryahin.v@gmail.com>
2017-10-30 19:51:59 +0300

    Changed read method

--- src/modules/Reader/Reader.js
+++ src/modules/Reader/Reader.js
@@ -9,16 +9,16 @@ export default class Reader {
     read() {
         const args = process.argv;
 
-        this._input = args[2] !== undefined ? Reader.readFromFile(args[2]) : Reader.readFromStdin();
-        this._output = args[3] !== undefined ? Reader.readFromFile(args[3]) : null;
+        try {
+            this._input = Reader.read(args[2]);
+            this._output = args[3] !== undefined ? Reader.read(args[3]) : null;
+        } catch (e) {
+            console.log('Файл не найден');
+        }
     }
 
-    static readFromFile(file) {
+    static read(file = '/dev/stdin') {
         return fs.readFileSync(file, 'utf8').toString();
-    }
-
-    static readFromStdin() {
-        return fs.readFileSync('/dev/stdin', 'utf8').toString();
     }
 
     get input() {
